29e9e8c99a5707c8126ad2ad43cf7f64b92e4db8
juan solano <juanpablosolanosuarez@hotmail.com>
2020-06-01 19:31:14 +0200

    autoplay

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -10,11 +10,11 @@ export class Video extends React.Component {
         <h1 className="banner-main-title">Programming Series in Amsterdam</h1>
         <p className="banner-title">Join our four week free Online Workshop</p>
         <video
+          playsInline
           className="video"
           loop
           muted
           autoPlay
-          playsInline
           src={
             'https://res.cloudinary.com/spread-the-love/video/upload/v1590921104/Le_Wagon_final_s4ch0g.mov'
           }
